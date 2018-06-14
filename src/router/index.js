5ac60346448a11fbc9450536bcae023ffed2f588
songxuehong <songxuehong@shangpin.com>
2018-06-14 12:15:30 +0800

    pro0.1

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 插入模板
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
